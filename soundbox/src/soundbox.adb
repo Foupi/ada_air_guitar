a2d86722576b18523cb9a98585ffb743471870dc
Thomas Michelot <thomas.michelot@epita.fr>
2021-02-14 19:09:15 +0100

    soundbox: fix continuous mix
    reference: https://open.spotify.com/track/0YYWw7n3J0kc28eWJLfntT?si=tBRQ9AQGS4qGLrkALCrQwg

--- soundbox/src/soundbox.adb
+++ soundbox/src/soundbox.adb
@@ -2,28 +2,36 @@ with Ada.Text_IO;   use Ada.Text_IO;
 with Ada.Streams;   use Ada.Streams;
 with Ada.Real_Time; use Ada.Real_Time;
 
-with Deserialization;  use Deserialization;
-with Note;             use Note;
-with Serial;           use Serial;
-with rt_note_player_h; use rt_note_player_h;
+with rt_note_player_h;
+with Deserialization; use Deserialization;
+with Note;            use Note;
+with Serial;          use Serial;
 
 procedure Soundbox is
 
    procedure Play_Note (N : Notes) is
    begin
-      Put_Line("Sending " & Notes'Pos (N)'Image);
+      Put_Line ("Sending " & Notes'Pos (N)'Image);
 
-      set_note (Notes'Pos (N));
+      rt_note_player_h.set_note (Notes'Pos (N));
+      delay 1.0; --  play for 1 second
+      rt_note_player_h.set_note (Notes'Pos (Nothing)); -- reset note
    end Play_Note;
+
+   procedure Audio_Initialization is
+   begin
+      Put_Line ("Setting up audio");
+      rt_note_player_h.audio_setup;
+   end Audio_Initialization;
 
    Current_Byte : Byte;
    Note         : Notes;
-
 begin
    SerialSetup;
-   audio_setup;
+   Audio_Initialization;
 
    loop
+      Put_Line ("");
       Put_Line ("Waiting for byte reception!");
 
       Current_Byte := Serial_Receive_Byte;
